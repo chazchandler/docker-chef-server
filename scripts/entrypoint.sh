0cc3ed76db75b9c148c9ff57adf54ed0a2258cc9
Chaz Chandler <clc31@inbox.com>
2018-02-04 01:47:44 +0000

    Added docker-compose support

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 set -x
+#ORG_NAME=${ORG_NAME:-my_org}
+#ORG_DESCRIPTION=${ORG_DESCRIPTION:-Default Org}
+ADMIN_USERNAME=${ADMIN_USERNAME:-admin}
+ADMIN_FIRSTNAME=${ADMIN_FIRSTNAME:-Default}
+ADMIN_LASTNAME=${ADMIN_LASTNAME:-Admin}
+ADMIN_EMAIL=${ADMIN_EMAIL:-admin@$ORG_NAME}
+echo "ADMIN_PASSWORD redacted"
 
 # For Postgres performance
 sysctl -w kernel.shmmax=17179869184
@@ -22,10 +29,10 @@ sysctl net.ipv6.conf.lo.disable_ipv6=0
 
 ## Create initial admin user if it is not existing
 if [[ $(chef-server-ctl user-list) =~ 'admin' ]]; then
-    echo "admin user is existing."
+    echo "admin user already exists."
 else
     # Create a default admin user
-    chef-server-ctl user-create admin admin admin admin@example.com 'admin123' --filename /etc/opscode/admin.pem
+    chef-server-ctl user-create ${ADMIN_USERNAME} ${ADMIN_FIRSTNAME} ${ADMIN_LASTNAME} ${ADMIN_EMAIL} "${ADMIN_PASSWORD}" --filename /etc/opscode/admin.pem
 
     # Reconfigure Chef Server
     chef-server-ctl reconfigure
